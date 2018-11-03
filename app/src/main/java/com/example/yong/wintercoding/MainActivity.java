707acf15b9a2e1e01f80f8df2adab805c45ed4d9
KuYong Kwon <smm1@naver.com>
2018-11-03 23:40:27 +0900

    REST요청후 JSON응답 받기

--- app/src/main/java/com/example/yong/wintercoding/MainActivity.java
+++ app/src/main/java/com/example/yong/wintercoding/MainActivity.java
@@ -1,13 +1,139 @@
 package com.example.yong.wintercoding;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "imagesearchexample";
+    public static final int LOAD_SUCCESS = 101;
+
+    private String SEARCH_URL = "https://secure.flickr.com/services/rest/?method=flickr.photos.search";
+    private String API_KEY = "&api_key=885b3adc7a2c372efd65ae5f27424f8a";
+    private String PER_PAGE = "&per_page=50";
+    private String SORT = "&sort=interestingness-desc";
+    private String FORMAT = "&format=json";
+    private String CONTECT_TYPE = "&content_type=1";
+    private String SEARCH_TEXT = "&text='cat'";
+    private String REQUEST_URL = SEARCH_URL + API_KEY + PER_PAGE + SORT + FORMAT + CONTECT_TYPE + SEARCH_TEXT;
+
+    private ProgressDialog progressDialog;
+    private TextView json_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button json_button = (Button)findViewById(R.id.json_button);
+        json_text = (TextView)findViewById(R.id.json_text);
+
+        json_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Please wait...");
+                progressDialog.show();
+
+                getJSON();
+            }
+        });
+
+    }
+
+    public void getJSON(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result;
+
+                try{
+                    Log.d(TAG, REQUEST_URL);
+                    URL url = new URL(REQUEST_URL);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+
+                    httpURLConnection.setReadTimeout(3000);
+                    httpURLConnection.setConnectTimeout(3000);
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.connect();
+
+                    int responseStatusCode = httpURLConnection.getResponseCode();
+
+                    InputStream inputStream;
+
+                    if (responseStatusCode == HttpURLConnection.HTTP_OK){
+                        inputStream = httpURLConnection.getInputStream();
+                    }else{
+                        inputStream = httpURLConnection.getErrorStream();
+                    }
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+
+                    while((line = bufferedReader.readLine()) != null){
+                        sb.append(line);
+                    }
+
+                    bufferedReader.close();
+                    httpURLConnection.disconnect();
+                    result = sb.toString().trim();
+
+                }catch (Exception e){
+                    result = e.toString();
+                }
+
+                Message message = myHandler.obtainMessage(LOAD_SUCCESS, result);
+                myHandler.sendMessage(message);
+
+            }
+        });
+
+        thread.start();
+    }
+
+    private final MyHandler myHandler = new MyHandler(this);
+
+    private static class MyHandler extends Handler{
+        private final WeakReference<MainActivity> weakReference;
+
+        public MyHandler(MainActivity mainActivity){
+            weakReference = new WeakReference<MainActivity>(mainActivity);
+        }
+
+        @Override
+        public void handleMessage(Message msg){
+            MainActivity mainActivity = weakReference.get();
+
+            if(mainActivity != null){
+                switch (msg.what){
+                    case LOAD_SUCCESS:
+                        mainActivity.progressDialog.dismiss();
+
+                        String jsonString = (String)msg.obj;
+
+                        mainActivity.json_text.setText(jsonString);
+                        break;
+                }
+            }
+        }
     }
 }
